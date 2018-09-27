dd26fa59e1d759454026786d01b1e4675bdf9299
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2018-09-27 01:55:01 -0400

    add

---
+++ pull_image_from_dockerhub_v1.11.0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+gcr_name=k8s.gcr.io
+myhub_name=zhaojiedi1992
+# define images 
+images=(
+	kube-apiserver-amd64:v1.11.0
+ kube-controller-manager-amd64:v1.11.0
+ kube-scheduler-amd64:v1.11.0
+ kube-proxy-amd64:v1.11.0
+ pause:3.1
+ etcd-amd64:3.2.18
+ coredns:1.1.3
+)
+
+for image in ${images[@]}; do 
+	docker pull $myhub_name/$image
+	docker tag  $myhub_name/$image $gcr_name/$image
+	#docer rmi $myhub_name/$image
+done
