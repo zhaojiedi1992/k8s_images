41db1efbffbb7dbae0c5b44afd19531f61c82ed4
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2018-10-06 01:28:12 -0400

    add dashboard

--- pull_image_from_dockerhub_v1.10.4.sh
+++ pull_image_from_dockerhub_v1.10.4.sh
@@ -4,6 +4,7 @@ gcr_name=k8s.gcr.io
 myhub_name=zhaojiedi1992
 # define images 
 images=(
+        kubernetes-dashboard-amd64:v1.10.0
 	kube-apiserver-amd64:v1.10.4
  kube-controller-manager-amd64:v1.10.4
  kube-scheduler-amd64:v1.10.4
